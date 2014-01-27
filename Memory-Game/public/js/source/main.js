28538bea18f989aadadea30a99115d681d9293f3
Tyler Malone <rtylermalone@gmail.com>
2014-01-27 01:21:29 -0600

    Commit of Memory Game

--- Memory-Game/public/js/source/main.js
+++ Memory-Game/public/js/source/main.js
@@ -5,32 +5,67 @@
   $(document).ready(init);
 
   function init(){
-    $('#play').click(randomNums);
+    $('#play').click(setBoard);
   }
+  var alphabet = ['A','B','C','D','E','F','G','H','I',
+    'J','K','L','M','N','O','P','Q','R','S','T','U',
+    'V','W','X','Y','Z'];
+  var cells = ['cell1','cell2','cell3','cell4','cell5',
+    'cell6','cell7','cell8','cell9','cell10',
+    'cell11','cell12','cell13','cell14','cell15',
+    'cell16','cell17','cell18','cell19','cell20'];
+  var slctLetters = [];
 
-  function randomNums(){
-    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXTZ';
-    var stringLen = 10;
-    var randomStr = '';
-    for (var i=0; i<stringLen; i++) {
+  function setBoard(){
+
+    function randomLetters(){
       var rnum = Math.floor(Math.random() * alphabet.length);
-      randomStr += alphabet.substring(rnum,rnum+1);
+      return rnum;
     }
-    debugger;
 
-    uniqueStr();
-
-    console.log(randomStr);
-    function uniqueStr(randomStr){
-      var unique = '';
-      for(var i=0; i<10; i++){
-        if(unique.indexOf(randomStr[i]) === -1){
-          unique += randomStr[i];
-        }
-        console.log(unique);
-      }
+    function randomNums(){
+      var rnum = Math.floor(Math.random() * cells.length);
+      return rnum;
     }
+
+    function randomLttr(){
+      var rnum = Math.floor(Math.random() * slctLetters.length);
+      return rnum;
+    }
+
+    for (var i=10; i>0; i--){
+      var number = randomLetters(alphabet.length - 1);
+      var chosen = alphabet[number];
+      slctLetters.push(chosen);
+      alphabet.splice(number, 1);
+    }
+
+    console.log(slctLetters);
+
+    var object = {};
+
+    for (var i2=10; i2>0; i2--){
+      var firstCell = randomNums(cells.length - 1);
+      var idOne = cells[firstCell];
+      var letter = randomLttr(slctLetters.length - 1);
+      object[idOne] = slctLetters[letter];
+      cells.splice(firstCell, 1);
+
+      var secondCell = randomNums(cells.length - 1);
+      var idTwo = cells[secondCell];
+      object[idTwo] = slctLetters[letter];
+      cells.splice(secondCell, 1);
+      slctLetters.splice(letter, 1);
+    }
+    console.log(object);
+
+    //for (var key in object) {
+    //  var text = object[key];
+   // }
+
+   // for (var ic=1; ic<21; ic++){
+   //   var text = object.text(ic);
+   //   $('.cell').append(text);
+   // }
   }
-
-
 })();
